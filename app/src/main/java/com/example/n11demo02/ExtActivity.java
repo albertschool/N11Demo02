78b0ee584a66b7c621ae8452085c23e1578a62ef
Albert <albert.school2015@gmail.com>
2023-06-14 15:05:20 +0300

    end of external. all working, need to fix design & add Javadoc

--- app/src/main/java/com/example/n11demo02/ExtActivity.java
+++ app/src/main/java/com/example/n11demo02/ExtActivity.java
@@ -6,15 +6,41 @@ import androidx.core.content.ContextCompat;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class ExtActivity extends MasterActivity {
     private static final int REQUEST_CODE_PERMISSION = 1;
+    private EditText eT;
+    private TextView tV;
+    private final String FILENAME = "exttest.txt";
+    private String strwr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ext);
+
+        initAll();
+    }
+
+    private void initAll() {
+        eT=(EditText)findViewById(R.id.eT);
+        tV=(TextView)findViewById(R.id.tV);
 
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -24,4 +50,73 @@ public class ExtActivity extends MasterActivity {
             Toast.makeText(this, "Permission to access external storage granted", Toast.LENGTH_LONG).show();
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission to access external storage granted", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Permission to access external storage NOT granted", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void write(View view) {
+        strwr=eT.getText().toString();
+        try {
+            File externalDir = Environment.getExternalStorageDirectory();
+            File file = new File(externalDir, FILENAME);
+            file.getParentFile().mkdirs();
+            FileWriter writer = new FileWriter(file);
+            writer.write(strwr);
+            writer.close();
+            Toast.makeText(this, "Text file saved successfully", Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Failed to save text file", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void reset(View view) {
+        try {
+            File externalDir = Environment.getExternalStorageDirectory();
+            File file = new File(externalDir, FILENAME);
+            file.getParentFile().mkdirs();
+            FileWriter writer = new FileWriter(file);
+            writer.write("");
+            writer.close();
+            Toast.makeText(this, "Text file cleared successfully", Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Failed to clear text file", Toast.LENGTH_SHORT).show();
+        }
+        eT.setText("");
+        tV.setText("");
+    }
+
+    public void read(View view) {
+        try {
+            File externalDir = Environment.getExternalStorageDirectory();
+            File file = new File(externalDir, FILENAME);
+            file.getParentFile().mkdirs();
+            FileReader reader = new FileReader(file);
+            BufferedReader bR = new BufferedReader(reader);
+
+            StringBuilder sB = new StringBuilder();
+            String line = bR.readLine();
+            while (line != null) {
+                sB.append(line+'\n');
+                line = bR.readLine();
+            }
+            tV.setText(sB.toString());
+            bR.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Failed to read text file", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
